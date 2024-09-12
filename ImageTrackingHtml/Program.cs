8a777e9a9524c86d19e2a7304dfbd9791b8f2074
Nikita Markelov <charliecheaters@yandex.ru>
2024-09-12 16:57:02 +0300

    Добавьте файлы проекта.

---
+++ ImageTrackingHtml/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Text;
+
+var curDomain = AppDomain.CurrentDomain.BaseDirectory;
+var path = "";
+bool isHorizonatal = false;
+DirectoryInfo di = null;
+do
+{
+
+    Console.WriteLine("Укажите путь до директории с изображениями");
+    Console.Write("Path: ");
+    path = Console.ReadLine();
+    di = new DirectoryInfo(path);
+    if (!di.Exists)
+    {
+        Console.WriteLine("Директория не существует");
+    }
+} while (!di.Exists);
+
+var imageUrl = "";
+var diskPath = "https://disk.yandex.ru/client/disk/";
+Console.WriteLine("Укажите сетевой адрес до папки с изображением");
+Console.Write("Web Path: ");
+imageUrl = Console.ReadLine();
+Console.Write("Horizontal Images? (Y/n): ");
+isHorizonatal = Console.ReadLine().ToLower().Any(x => x == 'y' || x == 'н');
+
+var tdStyle = isHorizonatal ? "horizontal-td" : "vertical-td";
+var imgStyle = isHorizonatal ? "horizontal-img" : "vertical-img";
+var columns = 3;
+var maxItemsPerPage = isHorizonatal ? 15 : 3;
+
+StringBuilder htmlBuilder = new StringBuilder();
+var extensions = new string[] { ".jpg", ".jpeg", ".png", ".svg" };
+var images = di.GetFiles("*.*",
+                 SearchOption.TopDirectoryOnly)
+                .Where(file => extensions.Contains(file.Extension))
+                .ToList();
+
+htmlBuilder.Append("<html><body>");
+htmlBuilder.AppendLine($"<style>{File.ReadAllText(Path.Combine(curDomain, "styles.css"))}</style>");
+htmlBuilder.Append("<table>");
+int count = 0;
+for (int i = 0; i < images.Count(); i++)
+{
+    if (count % columns == 0)
+    {
+        htmlBuilder.Append("<tr>");
+    }
+    var image = images[i];
+    htmlBuilder.Append($"<td class='{tdStyle}'>");
+    htmlBuilder.Append($"<img class='{imgStyle}' src='data:image/png;base64, {GetBase64(image.FullName)}' /> <br>");
+    htmlBuilder.Append($"{image.Name}<br>");
+    htmlBuilder.Append($"<a href='{diskPath + imageUrl}'>{imageUrl}");
+    htmlBuilder.Append("</td>");
+
+    count++;
+    if (count % columns == 0)
+    {
+        htmlBuilder.Append("</tr>");
+    }
+    if (count % maxItemsPerPage == 0)
+    {
+        htmlBuilder.Append("</table><table>");
+    }
+}
+
+htmlBuilder.Append("</table>");
+htmlBuilder.Append("</body></html>");
+
+File.WriteAllText(Path.Combine(curDomain, $"output {DateTime.Now.ToFileTime()}.html"), htmlBuilder.ToString());
+
+static string GetBase64(string imgPath)
+{
+    byte[] imageArray = System.IO.File.ReadAllBytes(imgPath);
+    return Convert.ToBase64String(imageArray);
+}
